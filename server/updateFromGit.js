adaf03602843be9966f3c53b09a70d66ea0f48e0
Thomas Doki-Thonon <dokithonon@gmail.com>
2015-08-29 09:43:05 +0200

    add console debug

--- server/updateFromGit.js
+++ server/updateFromGit.js
@@ -1,25 +1,27 @@
-if (Meteor.isServer){
+var child;
+var exec;
+var sys;
+var updateFromGit;
 
-  var exec = Npm.require('child_process').exec;
-  //var spawn = Npm.require('child_process').spawn;
-  var sys = Npm.require('sys');
+if (Meteor.isServer) {
+  exec = Npm.require('child_process').exec;
+  sys = Npm.require('sys');
 
   Meteor.methods({
-    updateFromGit : function (){
+    updateFromGit: function () {
       rkStatus.update(
 				{
-					method: 'updateFromGit'
+					method: 'updateFromGit',
 				},
 				{
 					method: 'updateFromGit',
-					state : 'running',
-					date: new Date()
+					state: 'running',
+					date: new Date(),
 				},
 				{
-					upsert: true
+					upsert: true,
 				}
 			);
-
       //download
       if (typeof(Meteor.settings.rationalK_Git.gitExePath) === 'undefined') {
           gitCommand = 'git';
@@ -28,81 +30,89 @@ if (Meteor.isServer){
         gitCommand = '"' + Meteor.settings.rationalK_Git.gitExePath + '"';
       }
 
-      dumpCommand = '(cd "'+ Meteor.settings.rationalK_Git.gitPullDir +'" && ' + gitCommand + ' pull)';
-      console.log(dumpCommand)
-      var child = exec(dumpCommand);
-
-      //Meteor.settings.rationalK_Git.autoUpdate;
-
+      dumpCommand = '(cd "' + Meteor.settings.rationalK_Git.gitPullDir + '" && ' + gitCommand + ' pull)';
+      if (Meteor.settings.public.debug) {
+        console.log(dumpCommand);
+      }
+      child = exec(dumpCommand);
 
       child.stdout.on('data', function (data) {
-        console.log('stdout: ' + data);
+        if (Meteor.settings.public.debug) {
+          console.log('stdout: ' + data);
+        }
       });
 
       child.stderr.on('data', function (data) {
-        console.log('stderr: ' + data);
-        if (data.indexOf("Server file no newer than local file") === 0){
-          //console.log('stderr: ' + data);
-          //child.serverFileNotNewerThanLocalFile =true;
-    		}
-
+        if (Meteor.settings.public.debug) {
+          console.log('stderr: ' + data);
+        }
       });
 
-
       child.on('close', function (code) {
-        console.log('child process exited with code ' + code);
-        if (code==0){
-            console.log("Files have been pulled, now we should deploy.");
-            if (process.env.NODE_ENV === "production"){
-              console.log("You are on production env, we should now re-deploy")
+        if (Meteor.settings.public.debug) {
+          console.log('child process exited with code ' + code);
+        }
+        if (code === 0) {
+          if (Meteor.settings.public.debug) {
+            console.log("Files have been pulled.");
+          }
+            if (process.env.NODE_ENV === "production") {
+              if (Meteor.settings.public.debug) {
+                console.log("You are on production env, we should now re-deploy");
+              }
               //deploy
               //dumpCommand = '(cd "' + Meteor.settings.rationalK_Git.mupDeployFolder + '" && mup deploy)';
-			  dumpCommand = '(cd "' + Meteor.settings.rationalK_Git.mupDeployFolder + '" && echo ' + Meteor.settings.rationalK_Git.sudoPassword + ' | sudo -S mup deploy)';
-              console.log(dumpCommand)
+			        dumpCommand = '(cd "' + Meteor.settings.rationalK_Git.mupDeployFolder + '" && echo ' + Meteor.settings.rationalK_Git.sudoPassword + ' | sudo -S mup deploy)';
+              if (Meteor.settings.public.debug) {
+                console.log(dumpCommand);
+              }
               childDeploy = exec(dumpCommand, function (error, stdout, stderr) {
-                if (stdout) {sys.print('stdout: ' + stdout);}
-                if (stderr) {sys.print('stderr: ' + stderr);}
-                if (error !== null) {console.log('exec error: ' + error);}
+                if (stdout) {sys.print('stdout: ' + stdout); }
+                if (stderr) {sys.print('stderr: ' + stderr); }
+                if (error !== null) {console.log('exec error: ' + error); }
               });
 
               rkStatus.update(
         				{
-        					method: 'updateFromGit'
+        					method: 'updateFromGit',
         				},
         				{
         					method: 'updateFromGit',
-        					state : 'finished',
-        					date: new Date()
+        					state: 'finished',
+        					date: new Date(),
         				},
         				{
-        					upsert: true
+        					upsert: true,
         				}
         			);
             }
             else {
-              console.log("You are on dev platform, it makes no sense to deploy. it will restart on its own...")
+              if (Meteor.settings.public.debug) {
+                console.log("You are on dev platform, it makes no sense to deploy. it will restart on its own...");
+              }
             }
         }
         else {
             //exit code diff de 0
+            if (Meteor.settings.public.debug) {
+              console.log(code);
+            }
         }
       });
-
-
-    }
+    },
   });
 
   if ( Meteor.settings.rationalK_Git.autoUpdate === true) {
-    var updateFromGit = function (){
+    updateFromGit = function () {
           Meteor.call('updateFromGit');
-    }
+    };
     SyncedCron.add({
       name: 'A cron that update rationalK from git',
       schedule: function (parser) {
         //return parser.text('at 12:30 pm');
         return parser.text('every 2 minutes');
       },
-      job: updateFromGit
+      job: updateFromGit,
     });
   }
 }
