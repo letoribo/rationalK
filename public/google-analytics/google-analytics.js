c62ab3cca0eed5cb9f9c5518b2ff4cfcbc6d94ef
Thomas Doki-Thonon <dokithonon@gmail.com>
2015-11-21 15:58:01 +0100

    remove bpm and sigmajs

--- public/google-analytics/google-analytics.js
+++ public/google-analytics/google-analytics.js
@@ -1,12 +1,8 @@
-if (document.URL.indexOf("demo.rationalk.ch") > -1){
+if (document.URL.indexOf("demo.rationalk.ch") > -1) {
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
 	ga('create', 'UA-56116002-2', 'auto');
 	ga('send', 'pageview');
-}else
-{
-	//console.log("I am not including google analytics code for this url (only for demo.rationalk.ch).");
 }
