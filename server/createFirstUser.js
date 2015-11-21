02027c871d334bd1bf912b94e17fdef93857ccc0
Thomas Doki-Thonon <dokithonon@gmail.com>
2015-11-21 17:54:34 +0100

    clean up

--- server/createFirstUser.js
+++ server/createFirstUser.js
@@ -1,26 +1,27 @@
-if (Meteor.isServer){
-  if (Members.collection.find({}).count()==0){
+var accountId;
+
+if (Meteor.isServer) {
+  if (Members.collection.find({}).count() === 0) {
     Meteor.users.remove({});
     Members.collection.remove({});
-    console.log('There is no user yet. Creating admin account...');
-    console.log('You can log using email : admin0@rationalk.ch / password : admin0');
+    RKCore.log('There is no user yet. Creating admin account...');
+    RKCore.log('You can log using email : admin0@rationalk.ch / password : admin0');
     //This is the default account for a fresh install
-    var accountId = Accounts.createUser({
+    accountId = Accounts.createUser({
         email: 'admin0@rationalk.ch',
         username: 'admin0',
         profile: {name: 'admin0', orgId: 'admin0'},
         password: 'admin0',
-        verified: true
+        verified: true,
       });
-
 
       Meteor.setTimeout(function () {
           Meteor.users.update({_id: accountId}, {
             $set: {
               username: 'admin0',
               profile: {name: 'admin0', orgId: 'admin0'},
-              emails : [ { address: 'admin0@rationalk.ch',verified: true } ]
-            }
+              emails: [ { address: 'admin0@rationalk.ch', verified: true } ],
+            },
           });
         }
         , 2000);
@@ -29,8 +30,8 @@ if (Meteor.isServer){
           Meteor.users.update({_id: accountId}, {
             $set: {
               username: 'admin',
-              profile: {name: 'admin', orgId: 'demo'}
-            }
+              profile: {name: 'admin', orgId: 'demo'},
+            },
           });
         }
         , 2000);
@@ -40,8 +41,8 @@ if (Meteor.isServer){
         orgId: 'demo',
         profile: {name: 'admin0', orgId: 'demo', roles: ['admin'], nickname: 'admin0'},
         username: 'admin0',
-        accountId: accountId
+        accountId: accountId,
       });
-      Roles.addUsersToRoles(accountId, ['admin'])
+      Roles.addUsersToRoles(accountId, ['admin']);
   }
 }
